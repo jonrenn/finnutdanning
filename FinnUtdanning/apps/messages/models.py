119ec02e8e410b5ba1c6122fb2e973cfe850a47a
jonren <jonren@stud.ntnu.no>
2019-03-12 12:41:47 +0100

    Adding chat class

--- FinnUtdanning/apps/messages/models.py
+++ FinnUtdanning/apps/messages/models.py
@@ -7,6 +7,9 @@ class Message(models.Model):
     #subject = models.Charfield('Subject: ', max_lenght=120)
     sent_at = models.DateTimeField(default=timezone.now())
     from_user = models.ForeignKey(Student)
-    to_user = models.ManyToManyField(Student)
+    to_chat = models.ForeignKey('Chat', related_name='+')
     message = models.CharField(max_length=500, blank=False)
 
+class Chat(models.Model):
+    participents = models.ManyToManyField(Student)
+    messages = models.ManyToManyField(Message)
